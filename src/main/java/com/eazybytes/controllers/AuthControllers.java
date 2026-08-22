69443c4b5986ce4526a6e26edc074b994933c8a9
spartan-khoatienbui <khoa.bui@c0x12c.com>
2026-08-22 18:45:32 +0700

    create auth controllers

--- src/main/java/com/eazybytes/controllers/AuthControllers.java
+++ src/main/java/com/eazybytes/controllers/AuthControllers.java
@@ -3,16 +3,21 @@ package com.eazybytes.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController()
-@RequestMapping("/user")
-public class UserControllers {
+@RestController
+@RequestMapping("/auth")
+public class AuthControllers {
 
-    @GetMapping("/me")
-    public ResponseEntity<String> getMe() {
+    @PostMapping("/login")
+    public ResponseEntity<String> login() {
         return ResponseEntity.status(HttpStatus.OK).body("OK");
     }
 
+    @GetMapping("/signup")
+    public ResponseEntity<String> signup() {
+        return ResponseEntity.status(HttpStatus.OK).body("OK");
+    }
 }
