51c3524c8062f91fa8f251c4798fc9605cc3b4d0
spartan-khoatienbui <khoa.bui@c0x12c.com>
2026-08-24 18:37:45 +0700

    setup SecurityConfig

---
+++ src/main/java/com/eazybytes/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.eazybytes.config;
+
+import com.eazybytes.constant.SecurityRoutes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
+        http.authorizeHttpRequests((requests) -> {
+            requests.requestMatchers(SecurityRoutes.getPublicRoutes()).permitAll();
+            requests.anyRequest().authenticated();
+        });
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.formLogin(AbstractHttpConfigurer::disable);
+        http.httpBasic(AbstractHttpConfigurer::disable);
+        http.exceptionHandling(exception -> exception.authenticationEntryPoint(
+                (request, response, authException) -> {
+                    response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                    response.getWriter().write("{\"message\":\"Unauthorized\"}");
+                }
+        ));
+        return http.build();
+    }
+
+}
---
+++ src/main/java/com/eazybytes/constant/SecurityRoutes.java
@@ -0,0 +1,18 @@
+package com.eazybytes.constant;
+
+public final class SecurityRoutes {
+
+    private final static String[] PUBLIC_ROUTES = {
+            "/api/auth/signup",
+            "/api/auth/login",
+            "/api/auth/forgot-password",
+            "/api/auth/reset-password"
+    };
+
+    private SecurityRoutes() {
+    }
+
+    public static String[] getPublicRoutes() {
+        return PUBLIC_ROUTES;
+    }
+}
